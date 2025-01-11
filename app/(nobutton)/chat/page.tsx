b6ac4ea7b0be88ec30dfae51a2019f67040d9e01
Riddhesh <riddhesh.chaudhary@gmail.com>
2025-01-11 22:26:05 +0530

    init

--- app/(nobutton)/chat/page.tsx
+++ app/(nobutton)/chat/page.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 const chat = () => {
   return (
-    <div>
+    <div className='w-full h-full'>
       {/* <iframe src="https://propstical-poc.streamlit.app/" width="100%" height="800px" frameBorder="0"></iframe> */}
       <iframe
-  src="https://30days.streamlit.app?embed=true"
-  style={{ height: '450px', width: '100%' }}
+  src="https://propstical-poc.streamlit.app?embed=true"
+  style={{ height: '1000px', width: '100%' }}
 ></iframe>
     </div>
   )
