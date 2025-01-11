177fc5fdd88c06303ca87522e57ce7bded0f6538
Riddhesh <riddhesh.chaudhary@gmail.com>
2025-01-11 22:19:11 +0530

    init-commit

---
+++ app/chat/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const chat = () => {
+  return (
+    <div>
+      <iframe src="https://propstical-poc.streamlit.app/" width="100%" height="800px" frameBorder="0"></iframe>
+      
+    </div>
+  )
+}
+
+export default chat
